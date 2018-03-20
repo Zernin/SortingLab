f317f186c68f4f30aa87dad0d595176d54a7177d
Jonathan <Jonathan@Jonathan>
2018-03-19 23:02:17 -0400

    Fixed Null Pointer Exception in EnteroTester

--- SortingAndExperimentation_Part1_Students/src/sortersTesterClasses/EnteroTester.java
+++ SortingAndExperimentation_Part1_Students/src/sortersTesterClasses/EnteroTester.java
@@ -8,6 +8,11 @@ public class EnteroTester {
 	
 	Entero[] arrOfEnteros = new Entero[3];
 	
+	arrOfEnteros[0] = new Entero(4); 
+	arrOfEnteros[1] = new Entero(3); 
+	arrOfEnteros[2] = new Entero(2); 
+	
+	
 	BubbleSortSorter<Entero> sorter = new BubbleSortSorter<Entero>();
 	
 	sorter.sort(arrOfEnteros, null);
